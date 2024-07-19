eecfad77ec37e333aea68a9302f7b0fac0d59ad8
Demidov Kirill <kirill.demidov.1995@mail.ru>
2024-07-19 17:20:28 +0300

    fix eslint errors

--- src/shared/components/AppButton/AppButton.test.tsx
+++ src/shared/components/AppButton/AppButton.test.tsx
@@ -3,13 +3,13 @@ import AppButton, { ButtonAppearance } from 'shared/components/AppButton/AppButt
 
 describe('AppButton', () => {
   test('AppButton render', () => {
-    render(<AppButton>Test AppButton</AppButton>);
+    render(<AppButton />);
 
     expect(screen.getByText('Test AppButton')).toBeInTheDocument();
   });
 
   test('AppButton clear appearance', () => {
-    render(<AppButton appearance={[ButtonAppearance.CLEAR]}>Test AppButton</AppButton>);
+    render(<AppButton appearance={[ButtonAppearance.CLEAR]} />);
 
     expect(screen.getByText('Test AppButton')).toHaveClass(ButtonAppearance.CLEAR);
   });
