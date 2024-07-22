7d246bee93d1d19d3f78e4dde6da71da1fe187c3
Demidov Kirill <kirill.demidov.1995@mail.ru>
2024-07-22 21:31:12 +0300

    minor fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -42,7 +42,7 @@ module.exports = {
     'react/jsx-indent': [2, 2],
     'react/jsx-indent-props': [2, 2],
     indent: [2, 2],
-    'linebreak-style': ['error', 'windows'],
+    'linebreak-style': 0,
     //    'linebreak-style': ['error', process.platform === 'win32' ? 'windows' : 'unix'],
   },
   globals: {
