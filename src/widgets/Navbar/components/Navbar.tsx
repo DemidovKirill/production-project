4605de970932f9268bcc3a1cd68dfe96ea9337a7
Demidov Kirill <kirill.demidov.1995@mail.ru>
2024-07-23 16:48:52 +0300

    updated sidebar and app-link

--- src/widgets/Navbar/components/Navbar.tsx
+++ src/widgets/Navbar/components/Navbar.tsx
@@ -1,20 +1,11 @@
 import { FunctionComponent } from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
-import { AppLink, AppLinkTheme } from 'shared/components/AppLink/AppLink';
-import { useTranslation } from 'react-i18next';
 import style from './style.module.scss';
 
 interface NavbarProps {
   className?: string
 }
 
-export const Navbar: FunctionComponent<NavbarProps> = ({ className }) => {
-  const { t } = useTranslation();
-
-  return (
-    <div className={classNames(style.navbar, {}, [className])}>
-      <AppLink theme={AppLinkTheme.PRIMARY} to="/">{t('mainPageLink')}</AppLink>
-      <AppLink theme={AppLinkTheme.SECONDARY} to="/about">{t('aboutPageLink')}</AppLink>
-    </div>
-  );
-};
+export const Navbar: FunctionComponent<NavbarProps> = ({ className }) => (
+  <div className={classNames(style.navbar, {}, [className])} />
+);
