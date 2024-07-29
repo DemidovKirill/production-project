758d7c0074548e34360fb1961bbd849949fbba14
Demidov Kirill <kirill.demidov.1995@mail.ru>
2024-07-29 21:43:35 +0300

    fixed loki test

--- src/widgets/Navbar/components/Navbar.stories.tsx
+++ src/widgets/Navbar/components/Navbar.stories.tsx
@@ -1,6 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { ThemeDecorator } from 'shared/config/storybook/decorators/theme-decorator';
 import { Theme } from 'shared/contexts/theme-context';
+import { StoreDecorator } from 'shared/config/storybook/decorators/store-decorator';
 import { Navbar } from './Navbar';
 
 const meta = {
@@ -14,11 +15,29 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Light: Story = {
+export const LightSingIn: Story = {
   args: {},
 };
-Light.decorators = [ThemeDecorator(Theme.LIGHT)];
+LightSingIn.decorators = [ThemeDecorator(Theme.LIGHT), StoreDecorator({
+  user: { userData: null },
+})];
+export const LightSingOut: Story = {
+  args: {},
+};
+LightSingOut.decorators = [ThemeDecorator(Theme.LIGHT), StoreDecorator({
+  user: { userData: { id: '1', username: 'admin' } },
+})];
 
-export const Dark: Story = {
+export const DarkSignIn: Story = {
   args: {},
 };
+DarkSignIn.decorators = [StoreDecorator({
+  user: { userData: null },
+})];
+
+export const DarkSignOut: Story = {
+  args: {},
+};
+DarkSignOut.decorators = [StoreDecorator({
+  user: { userData: { id: '1', username: 'admin' } },
+})];
