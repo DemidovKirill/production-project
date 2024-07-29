14a992c10841a8dde638a20d44e4e9caa62134a4
Demidov Kirill <kirill.demidov.1995@mail.ru>
2024-07-29 21:29:59 +0300

    fixed unit test

--- src/entities/User/models/selectors/getUserAuthData/getUserAuthData.test.ts
+++ src/entities/User/models/selectors/getUserAuthData/getUserAuthData.test.ts
@@ -5,7 +5,7 @@ import { getUserAuthData } from './getUserAuthData';
 describe('Get User Auth State', () => {
   test('Get User Auth State should return the correct counter state', () => {
     const state: DeepPartial<StoreSchema> = {
-      user: { userData: { id: '1', username: 'name' } },
+      user: { userData: { id: '1', username: 'admin' } },
     };
     expect(getUserAuthData(state as StoreSchema)).toEqual({
       id: '1',
