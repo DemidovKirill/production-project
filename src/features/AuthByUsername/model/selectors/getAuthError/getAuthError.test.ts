17f117ef5490d3e1b56f172969b4f789d473c274
Kirill Demidov <kirill.demidov.1995@mail.ru>
2024-07-31 18:29:08 +0300

    updated auth selectors test

--- src/features/AuthByUsername/model/selectors/getAuthError/getAuthError.test.ts
+++ src/features/AuthByUsername/model/selectors/getAuthError/getAuthError.test.ts
@@ -3,10 +3,15 @@ import { StoreSchema } from 'app/providers/Store';
 import { getAuthError } from './getAuthError';
 
 describe('Get Auth Error', () => {
-  test('Get Auth Error should return the correct counter state', () => {
+  test('Get Auth Error should return the correct error state', () => {
     const state: DeepPartial<StoreSchema> = {
       auth: { error: 'Error' },
     };
     expect(getAuthError(state as StoreSchema)).toEqual('Error');
+  });
+
+  test('Get Auth Error without state should return the correct error state', () => {
+    const state: DeepPartial<StoreSchema> = {};
+    expect(getAuthError(state as StoreSchema)).toEqual(null);
   });
 });
--- src/features/AuthByUsername/model/selectors/getAuthUsername/getAuthUsername.test.ts
+++ src/features/AuthByUsername/model/selectors/getAuthUsername/getAuthUsername.test.ts
@@ -3,10 +3,15 @@ import { StoreSchema } from 'app/providers/Store';
 import { getAuthUsername } from './getAuthUsername';
 
 describe('Get Auth Username', () => {
-  test('Get Auth Username should return the correct counter state', () => {
+  test('Get Auth Username should return the correct Username state', () => {
     const state: DeepPartial<StoreSchema> = {
       auth: { username: 'admin' },
     };
     expect(getAuthUsername(state as StoreSchema)).toEqual('admin');
+  });
+
+  test('Get Auth Username without state should return the correct Username state', () => {
+    const state: DeepPartial<StoreSchema> = {};
+    expect(getAuthUsername(state as StoreSchema)).toEqual('');
   });
 });
