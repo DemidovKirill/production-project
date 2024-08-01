0555074683181ac3c5cab6a82fb2e17e17be2864
Kirill Demidov <kirill.demidov.1995@mail.ru>
2024-08-01 12:57:54 +0300

    added auth slice test

---
+++ src/features/AuthByUsername/model/slice/auth.slice.test.ts
@@ -0,0 +1,16 @@
+import { authActions, authReducer, AuthSchema } from 'features/AuthByUsername';
+import { DeepPartial } from 'app/providers/Store';
+
+describe('Auth Slice', () => {
+  test('Set Username', () => {
+    const state: DeepPartial<AuthSchema> = { username: 'admin' };
+
+    expect(authReducer(state as AuthSchema, authActions.setUsername('new_admin'))).toEqual({ username: 'new_admin' });
+  });
+
+  test('Set Password', () => {
+    const state: DeepPartial<AuthSchema> = { password: '123' };
+
+    expect(authReducer(state as AuthSchema, authActions.setPassword('000'))).toEqual({ password: '000' });
+  });
+});
